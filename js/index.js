8172e4b1b59549583957da0a8e5ef07e819fade9
xZigzee <callum.w@hotmail.co.uk>
2016-03-08 13:57:41 +0000

    async function

--- js/index.js
+++ js/index.js
@@ -8,24 +8,32 @@ $(document).on('pagecreate', '#feedPage', function(event) {
 	<!-- The Yahoo pipe currently parses the BBC News RSS feed  -->
 	var xmlhttp=new XMLHttpRequest();
 	xmlhttp.open("GET", feedURL, true);
+	
+	xmlhttp.onreadystatechange = function() {
+			
+		alert("response = " + xmlhttp.responseText);
+			
+		<!-- parse the resulting JSON into Javascript Data Object -->
+		<!-- you can use a live parser to inspect the contents of the JSON
+		<!-- http://json.parser.online.fr/ -->
+		var weather= JSON.parse(xmlhttp.responseText);
+		
+		alert("response = " + weather);
+		
+		
+		<!-- Define Ractive binding -->
+		var ractive = new Ractive({
+	    	el: 'container', <!-- where -->
+	    	template: '#myTemplate', <!-- how -->
+	    	data: { weather : weather.consolidated_weather } <!-- what - specify the list of news articles 'items' using dot notation-->
+		});
+			
+	}
+	
+	
 	xmlhttp.send();
 		
 		
-	alert("response = " + xmlhttp.responseText);
-		
-	<!-- parse the resulting JSON into Javascript Data Object -->
-	<!-- you can use a live parser to inspect the contents of the JSON
-	<!-- http://json.parser.online.fr/ -->
-	var weather= JSON.parse(xmlhttp.responseText);
-	
-	alert("response = " + weather);
-	
-	
-	<!-- Define Ractive binding -->
-	var ractive = new Ractive({
-    	el: 'container', <!-- where -->
-    	template: '#myTemplate', <!-- how -->
-    	data: { weather : weather.consolidated_weather } <!-- what - specify the list of news articles 'items' using dot notation-->
-	});
+
 	
 });
